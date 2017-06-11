bd791672e27675f61c5fcf2602f953c463b6ecc6
Thomas Pakh <thomas.pakh@gmail.com>
2017-06-11 15:04:14 -0400

    First commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+Harder IC2 Coolent
+==================
+
+A tiny mod to remove the IC2 coolent recipe that uses regular water.
---
+++ src/main/java/com/tommsy/hardercoolent/HarderCoolent.java
@@ -0,0 +1,72 @@
+/*
+ * A tiny mod to remove the IC2 coolent recipe that uses regular water.
+ * Copyright (C) 2017  Thomas Pakh
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see LICENSE.md at the root of the project.
+ */
+package com.tommsy.hardercoolent;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.Logger;
+
+import ic2.api.recipe.ICannerEnrichRecipeManager.Input;
+import ic2.api.recipe.Recipes;
+import lombok.Getter;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = HarderCoolent.MOD_ID, name = HarderCoolent.MOD_NAME, version = HarderCoolent.VERSION)
+public class HarderCoolent {
+    public static final String MOD_ID = "@MOD_ID@";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final String VERSION = "@VERSION@";
+
+    @Getter
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        Map<Input, FluidStack> recipes = Recipes.cannerEnrich.getRecipes();
+        Set<Entry<Input, FluidStack>> entries = recipes.entrySet();
+        Iterator<Entry<Input, FluidStack>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Entry<Input, FluidStack> entry = iterator.next();
+            if (entry.getValue().getFluid().getName().equals("ic2coolant") &&
+                    entry.getKey().fluid.getFluid().getName().equals("water")) {
+                if (recipes.remove(entry.getKey(), entry.getValue())) {
+                    logger.info("Successfully removed coolent recipe.");
+                    return;
+                }
+            }
+        }
+        logger.error("Could not find coolent recipe.");
+        Collector<CharSequence, ?, String> joiner = Collectors.joining(",", "[", "]");
+        logger.error("Avaiable canner enrichment input fluids: {}", recipes.keySet().stream().map(i -> i.fluid.getFluid().getName()).collect(joiner));
+        logger.error("Avaiable canner enrichment output fluids: {}", recipes.values().stream().map(f -> f.getFluid().getName()).collect(joiner));
+    }
+}
